65caaadb37a263521bb2f1fdd44cdabc2e64bca1
Duda <maedchaves@gmail.com>
2026-04-12 19:26:43 -0300

    feat: adiciona função de subtração

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 def soma(a, b):
     return a + b
 
-# Projeto de Calculadora Simples para Prática de CI/CD
+def subtracao(a, b):
+    return a - b
+
 print("Calculadora Iniciada")
-print(f"Teste de soma (2 + 3): {soma(2, 3)}")
+print(f"Teste de soma (2+3): {soma(2, 3)}")
+print(f"Teste de subtração (10 - 4): {subtracao(10, 4)}")
