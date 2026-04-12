d7024578c4c7f3e35ed4a979d5342381d210a8fa
Duda <maedchaves@gmail.com>
2026-04-12 19:56:07 -0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# Projeto: Fluxo de CI/CD Prático
+# Desenvolvedor: Hen (Henrique)
+# Disciplina: DevOps
+
+def soma(a, b):
+    return a + b
+
+def subtracao(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print("Calculadora Iniciada")
+    print(f"Soma: {soma(10, 5)}")
+    print(f"Subtração: {subtracao(10, 5)}")
+
+print("Cálculo realizo com sucesso!")
